31e0dded342f7e30074fd55df4580600786cb598
kokbanyap <114297018+kokbanyap@users.noreply.github.com>
2022-10-08 00:55:46 +0800

    Upload files for Week 10 Homework

---
+++ vega_lite_vis.js
@@ -0,0 +1,10 @@
+var vg_1="GDPvsUnemploymentRate.vg.json";
+var vg_2="GlobalUnemploymentRate.vg.json";
+
+vegaEmbed("#scatter_plot",vg_1).then(function(result){
+
+}).catch(console.error);
+
+vegaEmbed("#choropleth_map",vg_2).then(function(result){
+
+}).catch(console.error);
